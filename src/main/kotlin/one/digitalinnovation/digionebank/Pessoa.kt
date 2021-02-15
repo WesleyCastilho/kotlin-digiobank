ab94728a93b341696a5f0bd92013f7757536caee
Wesley Castilho <wesley.castilho8@gmail.com>
2021-02-15 09:43:18 -0300

    Iniciando classes

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,5 +1,15 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Wesley"
+    var cpf: String = "123.345.678-9"
+}
+
+fun main(){
+    var wesley = Pessoa()
+
+    println("O nome é "+  wesley.nome)
+
+    println("O CPF é " + wesley.cpf)
 
 }
